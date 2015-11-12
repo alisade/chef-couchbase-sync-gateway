d57f7b79b71ce608617fdc4ebc572238061f15ab
Simon Bang Terkildsen <simon@monsenso.dk>
2015-11-12 10:42:16 +0100

    Refactor

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,34 +1,34 @@
-default['syncGateway']['edition'] = "community"
-default['syncGateway']['version'] = "1.1.0"
-default['syncGateway']['build'] = "28"
+default['sync_gateway']['edition'] = "community"
+default['sync_gateway']['version'] = "1.1.0"
+default['sync_gateway']['build'] = "28"
 
 case node['platform']
 when "windows"
-    default['syncGateway']['package_file'] = "couchbase-sync-gateway-#{node['syncGateway']['edition']}_#{node['syncGateway']['version']}-#{node['syncGateway']['build']}_x86_64.exe"
+    default['sync_gateway']['package_file'] = "couchbase-sync-gateway-#{node['sync_gateway']['edition']}_#{node['sync_gateway']['version']}-#{node['sync_gateway']['build']}_x86_64.exe"
 else
     Chef::Log.error("Couchbase Sync Gateway is not supported on #{node['platform_family']}")
 end
 
-default['syncGateway']['package_base_url'] = "http://packages.couchbase.com/releases/couchbase-sync-gateway/#{node['syncGateway']['version']}"
-default['syncGateway']['package_full_url'] = "#{node['syncGateway']['package_base_url']}/#{node['syncGateway']['package_file']}"
+default['sync_gateway']['package_base_url'] = "http://packages.couchbase.com/releases/couchbase-sync-gateway/#{default['sync_gateway']['version']}"
+default['sync_gateway']['package_full_url'] = "#{node['sync_gateway']['package_base_url']}/#{default['sync_gateway']['package_file']}"
 
 case node['platform_family']
 when "windows"
-  default['syncGateway']['install_dir'] = File.join("C:","Program Files (x86)","CouchbaseSyncGateway")
+  default['sync_gateway']['install_dir'] = 'C:\Program Files (x86)\CouchbaseSyncGateway'
 else
-  default['syncGateway']['install_dir'] = "/opt/couchbase-sync-gateway"
+  default['sync_gateway']['install_dir'] = "/opt/couchbase-sync-gateway"
 end
 
-default['syncGateway']['service_name'] = 'CouchbaseSyncGateway'
+default['sync_gateway']['service_name'] = 'CouchbaseSyncGateway'
 
-default['syncGateway']['adminInterface'] = '127.0.0.1:4985'
-default['syncGateway']['adminUI'] = '127.0.0.1:4985/_admin/'
-default['syncGateway']['compressResponses'] = true
-default['syncGateway']['interface'] = ':4984'
-default['syncGateway']['log'] = ['HTTP']
-default['syncGateway']['logFilePath'] = 'stderr'
-default['syncGateway']['maxCouchbaseChttponnections'] = 16
-default['syncGateway']['maxFileDescriptors'] = 5000
-default['syncGateway']['pretty'] = false
-default['syncGateway']['slowServerCallWarningThreshold'] = 200
+default['sync_gateway']['adminInterface'] = '127.0.0.1:4985'
+default['sync_gateway']['adminUI'] = '127.0.0.1:4985/_admin/'
+default['sync_gateway']['compressResponses'] = true
+default['sync_gateway']['interface'] = ':4984'
+default['sync_gateway']['log'] = ['HTTP', 'Auth', 'Access', 'Shadow']
+default['sync_gateway']['logFilePath'] = 'stderr'
+default['sync_gateway']['maxCouchbaseChttponnections'] = 16
+default['sync_gateway']['maxFileDescriptors'] = 5000
+default['sync_gateway']['pretty'] = false
+default['sync_gateway']['slowServerCallWarningThreshold'] = 200
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,31 +1,31 @@
 include_recipe 'nssm'
 
-remote_file File.join(Chef::Config[:file_cache_path], node['syncGateway']['package_file']) do
-  source node['syncGateway']['package_full_url']
+remote_file File.join(Chef::Config[:file_cache_path], node['sync_gateway']['package_file']) do
+  source node['sync_gateway']['package_full_url']
   action :create_if_missing
 end
 
 case node['platform']
 when 'windows'
     windows_package 'Couchbase Sync Gateway' do
-        source File.join(Chef::Config[:file_cache_path], node['syncGateway']['package_file'])
-        options "/s /v\"/qn AgreeToLicense=Yes INSTALLDIR=\\\"#{node['syncGateway']['install_dir'].gsub('/','\\\\')}\\\"\""
+        source File.join(Chef::Config[:file_cache_path], node['sync_gateway']['package_file'])
+        options "/s /v\"/qn AgreeToLicense=Yes INSTALLDIR=\\\"#{node['sync_gateway']['install_dir'].gsub('/','\\\\')}\\\"\""
         installer_type :custom
         action :install
     end
 end
 
-template "#{node['syncGateway']['install_dir']}/config.json" do
+template "#{node['sync_gateway']['install_dir']}/config.json" do
     source 'config.json.erb'
     action :create
-    notifies :restart, "windows_service[#{node['syncGateway']['service_name']}]"
+    notifies :restart, "windows_service[#{node['sync_gateway']['service_name']}]"
 end
 
 case node['platform']
 when 'windows'
-    nssm "#{node['syncGateway']['service_name']}" do
-        program "#{node['syncGateway']['install_dir']}\\syncGateway.exe"
-        args %("""#{node['syncGateway']['install_dir']}\\config.json""")
+    nssm "#{node['sync_gateway']['service_name']}" do
+        program "#{node['sync_gateway']['install_dir']}\\sync_gateway.exe"
+        args 'config.json'
         params(
             # DependOnService: 'CouchbaseServer',
             Start: 'SERVICE_DELAYED_AUTO_START'
@@ -34,7 +34,7 @@ when 'windows'
         action :install
     end
 
-    windows_service "#{node['syncGateway']['service_name']}" do
+    windows_service "#{node['sync_gateway']['service_name']}" do
         :start
     end
 end
